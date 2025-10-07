dd1c2f5131ca6b1e7faf1e5c4e8273ad52ca4297
Sopho Gurjishvili <123485326+sophyrise@users.noreply.github.com>
2025-10-07 19:31:45 +0400

    picture visuals corrected

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import mainArt from './react-icons/mainPhoto.svg'
+import underArt from './react-icons/underPhoto.svg'
 import FaqItems from './faq-items'
 
 function App() {
   return (
     <div className="page-gradient">
+      <img className="mainPhoto" src={mainArt} alt="Illustration1" />
+      <img className="underPhoto" src={underArt} alt="Illustration2" />
+
       <div className="faq-card">
-
-        <div className="faq-art">
-          <img src={mainArt} alt="Illustration" />
-        </div>
-
         <div className="faq-content">
 
           <h1 className="faq-title">FAQ</h1>
